3d57c43d0b2009ee68e4b8ce60d1fda05bca3514
Alumno <Alumno@10.76.179.133>
2018-03-03 11:10:36 -0600

    Autor: Juan Ernesto Descripcion: Commit inicial

---
+++ src/pruebapacman/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebapacman;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
