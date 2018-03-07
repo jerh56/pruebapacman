55fb2c87ac9ef40d030c4813cff460b9dee6548e
brandontm <brandon_btm@outlook.com>
2018-03-07 15:30:05 -0700

    Agregar un mensaje más También se actualizo el .gitignore

--- src/pruebapacman/Principal.java
+++ src/pruebapacman/Principal.java
@@ -1,24 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pruebapacman;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Alumno
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hola Mundo");
         System.out.println("Mensaje escrito por Brandon");
         JOptionPane.showMessageDialog(null, "Hola Mundo");
@@ -29,12 +16,16 @@ public class Principal {
         System.out.println("Hola soy el diseñador");
         System.out.println("._.");
         Persona alumno1 = new Persona("Carlos", "Contreras", 19);
-        System.out.println("El alumno " + alumno1.getNombre() + " " + alumno1.getApellido() + " tiene " + alumno1.getEdad() + " años.");
+        System.out.println("El alumno ");
+        for(char c : ((alumno1.getNombre() + " ").concat(alumno1.getApellido())).toCharArray()) {
+            System.out.println(c);
+        }
+        System.out.println("tiene " + alumno1.getEdad() + " años.");
         System.out.println("█║▌│█│║▌║││█║▌║▌║ "); // Carlos Contreras
         System.out.println("Este Pac-Man va directo a Steam");
         System.out.println("Alguien aquí sabe programar PIC con XC8+MPLAB X? Se agradece cualquier pista xD -Armando Payán"); 
         
-       /*
+        /*
         Ahora, un Nyan Cat en ASCII :D
         */ 
        
