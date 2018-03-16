b9cbcdaf0a004ca3d97be5da51aabb5edaeb31c5
Brandon Tirado <brandon_btm@outlook.com>
2018-03-15 21:52:34 -0700

    Agregar clase Direction

---
+++ src/clases/Direction.java
@@ -0,0 +1,27 @@
+package clases;
+
+public enum Direction {
+    UP(1),
+    RIGHT(2),
+    DOWN(3),
+    LEFT(4);
+
+    private final int value;
+
+    Direction(int value) {
+        this.value = value;
+    }
+
+    public int getValue() { return value; }
+
+    public Direction getDirection(int value) {
+        Direction dir = null;
+
+        if(value == 1) dir = Direction.UP;
+        if(value == 2) dir = Direction.RIGHT;
+        if(value == 3) dir = Direction.DOWN;
+        if(value == 4) dir = Direction.LEFT;
+
+        return dir;
+    }
+}
