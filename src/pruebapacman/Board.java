6d549011d8b6b4cac34276d6c629c43336f8db19
Johnny <jerh56@gmail.com>
2018-03-22 23:58:26 -0700

    Descripción: Modifiqué una tercera parte del laberinto para que sea más parecido al juego original

--- src/pruebapacman/Board.java
+++ src/pruebapacman/Board.java
@@ -115,6 +115,24 @@ public class Board extends JPanel implements ActionListener {
         17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
         17, 16, 24, 24, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
         17, 20,  0,  0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        17, 16, 18, 18,  0, 16, 24, 24, 24, 16, 16, 16, 16, 16, 20,
+        17, 16, 16, 16, 16, 21,  0,  0,  0, 21, 16, 16, 16, 16, 20,
+        17, 16, 16, 16, 24, 24, 22,  0, 19, 24, 24, 16, 16, 16, 20,
+        17, 16, 16, 20,  0,  0, 21,  0, 21,  0,  0, 17, 16, 16, 20,
+        17, 16, 16, 16, 26, 18, 24, 26, 24, 18, 26, 16, 32, 16, 20,
+        17, 16, 16, 20,  0, 21,  0,  0,  0, 21,  0, 17, 16, 16, 20,
+        17, 24, 24, 28,  0, 25, 22,  0, 19, 28,  0, 25, 24, 24, 20,
+        21,  0,  0,  0,  0,  0, 21,  0, 21,  0,  0,  0,  0,  0, 21,
+        25, 26, 26, 26, 26, 26, 24, 26, 24, 26, 26, 26, 26, 26, 28
+        };
+    
+    private final short levelData2[] = {
+        51, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 54,
+        17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        17, 16, 24, 24, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        17, 20,  0,  0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
         17, 16, 18,  18,  0, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
         17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
         17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
@@ -125,25 +143,6 @@ public class Board extends JPanel implements ActionListener {
         17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
         57, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 60
         };
-    
-//        21, 0, 0, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-//        21, 0, 0, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-//        21, 0, 0, 0, 17, 16, 16, 24, 16, 16, 16, 16, 16, 16, 20,
-//        17, 18, 18, 18, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 20,
-//        17, 16, 16, 16, 16, 16, 20, 0, 17, 16, 16, 16, 16, 24, 20,
-//        25, 16, 16, 16, 24, 24, 28, 0, 25, 24
-//            ,
-//        
-//        24, 16, 20, 0, 21,
-//        1, 17, 16, 20, 0, 0, 0, 0, 0, 0, 0, 17, 20, 0, 21,
-//        1, 17, 16, 16, 18, 18, 22, 0, 19, 18, 18, 16, 20, 0, 21,
-//        1, 17, 16, 16, 16, 16, 20, 0, 17, 16, 16, 16, 20, 0, 21,
-//        1, 17, 16, 16, 16, 16, 20, 0, 17, 16, 16, 16, 20, 0, 21,
-//        1, 17, 16, 16, 16, 16, 16, 18, 16, 16, 16, 16, 20, 0, 21,
-//        1, 17, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0, 21,
-//        1, 25, 24, 24, 24, 24, 24, 24, 24, 24, 16, 16, 16, 18, 20,
-//        9, 8, 8, 8, 8, 8, 8, 8, 8, 8, 25, 24, 24, 24, 28
-//    };
     
     private final int validSpeeds[] = {1, 2, 3, 4, 6, 8};
     private final int maxSpeed = 6;
@@ -331,74 +330,7 @@ public class Board extends JPanel implements ActionListener {
 
         short i;
         int pos;
-        int count;
-
-//        for (i = 0; i < N_GHOSTS; i++) {
-//            if (ghost_x[i] % BLOCK_SIZE == 0 && ghost_y[i] % BLOCK_SIZE == 0) {
-//                pos = ghost_x[i] / BLOCK_SIZE + N_BLOCKS * (int) (ghost_y[i] / BLOCK_SIZE);
-//
-//                count = 0;
-//
-//                if ((screenData[pos] & 1) == 0 && ghost_dx[i] != 1) {
-//                    dx[count] = -1;
-//                    dy[count] = 0;
-//                    count++;
-//                }
-//
-//                if ((screenData[pos] & 2) == 0 && ghost_dy[i] != 1) {
-//                    dx[count] = 0;
-//                    dy[count] = -1;
-//                    count++;
-//                }
-//
-//                if ((screenData[pos] & 4) == 0 && ghost_dx[i] != -1) {
-//                    dx[count] = 1;
-//                    dy[count] = 0;
-//                    count++;
-//                }
-//
-//                if ((screenData[pos] & 8) == 0 && ghost_dy[i] != -1) {
-//                    dx[count] = 0;
-//                    dy[count] = 1;
-//                    count++;
-//                }
-//
-//                if (count == 0) {
-//
-//                    if ((screenData[pos] & 15) == 15) {
-//                        ghost_dx[i] = 0;
-//                        ghost_dy[i] = 0;
-//                    } else {
-//                        ghost_dx[i] = -ghost_dx[i];
-//                        ghost_dy[i] = -ghost_dy[i];
-//                    }
-//
-//                } else {
-//
-//                    count = (int) (Math.random() * count);
-//
-//                    if (count > 3) {
-//                        count = 3;
-//                    }
-//
-//                    ghost_dx[i] = dx[count];
-//                    ghost_dy[i] = dy[count];
-//                }
-//
-//            }
-//
-//            ghost_x[i] = ghost_x[i] + (ghost_dx[i] * ghostSpeed[i]);
-//            ghost_y[i] = ghost_y[i] + (ghost_dy[i] * ghostSpeed[i]);
-//            drawGhost(g2d, ghost_x[i] + 1, ghost_y[i] + 1, i);
-//
-//            if (pacman_x > (ghost_x[i] - 12) && pacman_x < (ghost_x[i] + 12)
-//                    && pacman_y > (ghost_y[i] - 12) && pacman_y < (ghost_y[i] + 12)
-//                    && inGame) {
-//
-//                dying = true;
-//            }
-//        }
-        
+        int count;        
         for (i = 0; i < N_GHOSTS; i++) {
             if (fantasmas.get(i).getPosx() % BLOCK_SIZE == 0 && fantasmas.get(i).getPosy() % BLOCK_SIZE == 0) {
                 pos = fantasmas.get(i).getPosx() / BLOCK_SIZE + N_BLOCKS * (int) (fantasmas.get(i).getPosy() / BLOCK_SIZE);
@@ -663,7 +595,7 @@ public class Board extends JPanel implements ActionListener {
 //        }
 
         pacman_x = 7 * BLOCK_SIZE;
-        pacman_y = 11 * BLOCK_SIZE;
+        pacman_y = 10 * BLOCK_SIZE;
         pacmand_x = 0;
         pacmand_y = 0;
         req_dx = 0;
